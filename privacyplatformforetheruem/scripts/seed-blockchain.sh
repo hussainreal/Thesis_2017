459d4b9d584e569b56d3ac30112cef7798f2ca90
Ubuntu <thesis_blockchain@Ethnode1.tw5mk1zfr2qubhxfcge4rnnrcf.vx.internal.cloudapp.net>
2017-12-28 22:14:03 +0000

    hello

--- privacyplatformforetheruem/scripts/seed-blockchain.sh
+++ privacyplatformforetheruem/scripts/seed-blockchain.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-geth --nodiscover --maxpeers 1 --identity "EthNode2"  --rpc --rpcport "8080" --rpccorsdomain "*"  --port "30303" --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --networkid 496 --datadir "chains/devtest" --targetgaslimit 60000000 init lab-seed.json
+geth --datadir chains/devtest init lab-seed.json
 
-geth --datadir chains/devtest
 
